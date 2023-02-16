befff87cf2ea38a48fd94b53a2d9032040f479ed
petrostegegn <petrostegegn0@gmail.com>
2023-02-16 01:17:45 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+{
 	printf("size of a char: %d byte(s)\n", sizeof(char));
 	printf("size of an int: %d byte(s)\n", sizeof(int));
 	printf("size of a long int: %d byte(s)\n", sizeof(long int));
