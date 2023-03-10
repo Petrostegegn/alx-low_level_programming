749f029d48a6452211d37eb2aa071722a29dac23
petrostegegn <petrostegegn0@gmail.com>
2023-03-10 03:48:24 -0800

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,15 +15,10 @@ int _atoi(char *s)
 int i, d, n, len, f, digit;
 
 i = 0;
-
 d = 0;
-
 n = 0;
-
 len = 0;
-
 f = 0;
-
 digit = 0;
 
 while (s[len] != '\0')
@@ -47,9 +42,7 @@ digit = s[i] - '0';
 if (d % 2)
 
 digit = -digit;
-
 n = n * 10 + digit;
-
 f = 1;
 
 if (s[i + 1] < '0' || s[i + 1] > '9')
